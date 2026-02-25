20b513018fcd8eb86b36a6b0b6343d87d5d72eab
Pacifique16 <harerimanapacifique95@gmail.com>
2026-02-26 00:31:47 +0200

    Update all form labels with proper capitalization

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace AUCASAPRO_26937
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label7 = new Label();
+			button4 = new Button();
+			button3 = new Button();
+			button2 = new Button();
+			button1 = new Button();
+			dataGridView1 = new DataGridView();
+			textBox6 = new TextBox();
+			textBox5 = new TextBox();
+			textBox4 = new TextBox();
+			textBox3 = new TextBox();
+			textBox2 = new TextBox();
+			textBox1 = new TextBox();
+			label6 = new Label();
+			label5 = new Label();
+			label4 = new Label();
+			label3 = new Label();
+			label2 = new Label();
+			label1 = new Label();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+			SuspendLayout();
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			label7.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label7.Location = new Point(342, -27);
+			label7.Name = "label7";
+			label7.Size = new Size(156, 25);
+			label7.TabIndex = 35;
+			label7.Text = "APPLY HERE";
+			// 
+			// button4
+			// 
+			button4.BackColor = Color.RosyBrown;
+			button4.Location = new Point(509, 192);
+			button4.Name = "button4";
+			button4.Size = new Size(94, 29);
+			button4.TabIndex = 34;
+			button4.Text = "UPDATE";
+			button4.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			button3.BackColor = Color.FromArgb(128, 128, 255);
+			button3.Location = new Point(509, 136);
+			button3.Name = "button3";
+			button3.Size = new Size(94, 29);
+			button3.TabIndex = 33;
+			button3.Text = "CANCEL";
+			button3.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			button2.BackColor = Color.FromArgb(255, 255, 192);
+			button2.Location = new Point(509, 78);
+			button2.Name = "button2";
+			button2.Size = new Size(94, 29);
+			button2.TabIndex = 32;
+			button2.Text = "CHECK";
+			button2.UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			button1.BackColor = Color.FromArgb(255, 192, 192);
+			button1.Location = new Point(509, 35);
+			button1.Name = "button1";
+			button1.Size = new Size(94, 29);
+			button1.TabIndex = 31;
+			button1.Text = "APPLY";
+			button1.UseVisualStyleBackColor = false;
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.BackgroundColor = Color.Silver;
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Location = new Point(64, 345);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.RowHeadersWidth = 51;
+			dataGridView1.Size = new Size(677, 132);
+			dataGridView1.TabIndex = 30;
+			// 
+			// textBox6
+			// 
+			textBox6.Location = new Point(196, 286);
+			textBox6.Name = "textBox6";
+			textBox6.Size = new Size(183, 27);
+			textBox6.TabIndex = 29;
+			// 
+			// textBox5
+			// 
+			textBox5.Location = new Point(196, 237);
+			textBox5.Name = "textBox5";
+			textBox5.Size = new Size(183, 27);
+			textBox5.TabIndex = 28;
+			// 
+			// textBox4
+			// 
+			textBox4.Location = new Point(196, 189);
+			textBox4.Name = "textBox4";
+			textBox4.Size = new Size(183, 27);
+			textBox4.TabIndex = 27;
+			// 
+			// textBox3
+			// 
+			textBox3.Location = new Point(196, 140);
+			textBox3.Name = "textBox3";
+			textBox3.Size = new Size(183, 27);
+			textBox3.TabIndex = 26;
+			// 
+			// textBox2
+			// 
+			textBox2.Location = new Point(196, 82);
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(183, 27);
+			textBox2.TabIndex = 25;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(196, 20);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(183, 27);
+			textBox1.TabIndex = 24;
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Location = new Point(59, 286);
+			label6.Name = "label6";
+			label6.Size = new Size(63, 20);
+			label6.TabIndex = 23;
+			label6.Text = "position";
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Location = new Point(59, 237);
+			label5.Name = "label5";
+			label5.Size = new Size(112, 20);
+			label5.TabIndex = 22;
+			label5.Text = "current average";
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(59, 196);
+			label4.Name = "label4";
+			label4.Size = new Size(68, 20);
+			label4.TabIndex = 21;
+			label4.Text = "semester";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(59, 140);
+			label3.Name = "label3";
+			label3.Size = new Size(87, 20);
+			label3.TabIndex = 20;
+			label3.Text = "department";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(59, 82);
+			label2.Name = "label2";
+			label2.Size = new Size(46, 20);
+			label2.TabIndex = 19;
+			label2.Text = "name";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(59, 20);
+			label1.Name = "label1";
+			label1.Size = new Size(75, 20);
+			label1.TabIndex = 18;
+			label1.Text = "student id";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(label7);
+			Controls.Add(button4);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(dataGridView1);
+			Controls.Add(textBox6);
+			Controls.Add(textBox5);
+			Controls.Add(textBox4);
+			Controls.Add(textBox3);
+			Controls.Add(textBox2);
+			Controls.Add(textBox1);
+			Controls.Add(label6);
+			Controls.Add(label5);
+			Controls.Add(label4);
+			Controls.Add(label3);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Name = "Form1";
+			Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label label7;
+		private Button button4;
+		private Button button3;
+		private Button button2;
+		private Button button1;
+		private DataGridView dataGridView1;
+		private TextBox textBox6;
+		private TextBox textBox5;
+		private TextBox textBox4;
+		private TextBox textBox3;
+		private TextBox textBox2;
+		private TextBox textBox1;
+		private Label label6;
+		private Label label5;
+		private Label label4;
+		private Label label3;
+		private Label label2;
+		private Label label1;
+	}
+}
