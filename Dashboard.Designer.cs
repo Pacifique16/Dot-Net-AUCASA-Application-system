d928477c9436628f8c4c19e3971a740eedd9bfef
Pacifique Harerimana <harerimanapacifique95@gmail.com>
2026-02-28 23:42:21 +0200

    Add WhiteSmoke background color to Dashboard form

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -255,6 +255,7 @@ namespace AUCASAPRO_26937
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.WhiteSmoke;
 			this.ClientSize = new System.Drawing.Size(920, 480);
 			this.Controls.Add(this.panel5);
 			this.Controls.Add(this.panel4);
